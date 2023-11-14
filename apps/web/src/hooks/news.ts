2b7223ea7298e2cf2c83c14759a7ee3de18cdc9e
Erick Vargas <erianvc.dev@gmail.com>
2023-11-14 15:47:18 -0500

    feat(web): adds infinity scroll to get more news

--- apps/web/src/hooks/news.ts
+++ apps/web/src/hooks/news.ts
@@ -6,25 +6,46 @@ import type { MlCardProps } from 'atomic/molecules/ml-card'
 import type { APINewsResponse } from '@/types/api'
 
 interface UseNewsServiceReturn {
-  cards: MlCardProps[]
   loading: boolean
+  cards: MlCardProps[]
+  getMoreNewsByCategory: GetMoreNewsByCategory
 }
-type GetNews = (params: { page: number }) => void
+type GetNews = (params: { category: string | null }) => void
+type GetMoreNewsByCategory = (category: string | null) => void
 
 export function useNewsService (): UseNewsServiceReturn {
+  const TOTAL_PAGES = 50
   const [cards, setCards] = useState<MlCardProps[]>([])
   const [loading, setLoading] = useState<boolean>(true)
+  const [currentPage, setCurrentPage] = useState<number>(0)
+  const [currentCategory, setCurrentCategory] = useState<string | null>(null)
 
-  useEffect(() => { getNews({ page: 0 }) }, [])
+  useEffect(() => { getNews({ category: null }) }, [])
+  useEffect(() => {
+    const triggerAction = (): void => {
+      const { body } = document
+      const limit = Math.max(body.scrollHeight, body.offsetHeight, body.clientHeight)
+      const scrolled = window.scrollY + window.innerHeight
+      const isOnBottom = (limit - 200) <= scrolled
 
-  const getNews: GetNews = ({ page }) => {
+      if (!loading && isOnBottom) getMoreNewsByCategory(currentCategory)
+    }
+
+    window.addEventListener('scroll', triggerAction)
+    return () => { window.removeEventListener('scroll', triggerAction) }
+  }, [loading, currentCategory])
+
+  const getNews: GetNews = (category) => {
+    const isOnLastPage = currentPage === TOTAL_PAGES
+
     setLoading(true)
-    if (typeof API_URL !== 'string') return []
+    if (typeof API_URL !== 'string' || isOnLastPage) return []
 
-    const newsAPIUrl = new URL(API_URL)
-    newsAPIUrl.searchParams.append('page', page.toString())
+    const newsApiUrl = new URL(API_URL)
+    if (typeof category === 'string') newsApiUrl.searchParams.append('category', category)
+    newsApiUrl.searchParams.append('page', currentPage.toString())
 
-    fetch(newsAPIUrl)
+    fetch(newsApiUrl)
       .then(response => {
         response.json()
           .then(({ hits }: APINewsResponse) => {
@@ -36,13 +57,22 @@ export function useNewsService (): UseNewsServiceReturn {
               createdAt: getRelativeTimeFromNow(hit.created_at_i)
             }))
 
-            setCards(news)
+            setCards(prevNews => [...prevNews, ...news])
           })
           .catch((error) => { console.error(error) })
       })
       .catch((error) => { console.error(error) })
-      .finally(() => { setLoading(false) })
+      .finally(() => {
+        setCurrentPage(prevPage => prevPage + 1)
+        setLoading(false)
+      })
   }
 
-  return { cards, loading }
+  const getMoreNewsByCategory: GetMoreNewsByCategory = (category) => {
+    if (currentCategory !== category) setCurrentPage(0)
+    setCurrentCategory(category)
+    getNews({ category })
+  }
+
+  return { cards, loading, getMoreNewsByCategory }
 }
